0a10ea2f9039ebff180f8841579428091dc84a5e
Eric Holzer <eric.holzer.lsn@gmail.com>
2023-11-25 17:07:39 +0100

    Add CPP01 ex03

---
+++ CPP01/ex03/srcs/HumanB.cpp
@@ -0,0 +1,16 @@
+#include "HumanB.hpp"
+#include "iostream"
+
+HumanB::HumanB(std::string name) : _name(name) {
+}
+
+void	HumanB::attack() const {
+	if (!this->_weapon)
+		std::cout << this->_name << " attacks with their nothing" << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+}
+
+void	HumanB::setWeapon(Weapon &weapon) {
+	this->_weapon = &weapon;
+}
