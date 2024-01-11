586f57a1b7a769281499d26593bdb083a2301b6c
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-01-11 18:48:19 +0100

    Add CPP07 ex00

---
+++ CPP07/ex00/incl/whatever.hpp
@@ -0,0 +1,21 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T &arg1, T &arg2) {
+	T temp = arg1;
+	arg1 = arg2;
+	arg2 = temp;
+}
+
+template <typename T>
+T & min(T &arg1, T &arg2) {
+	return arg1 < arg2 ? arg1 : arg2;
+}
+
+template <typename T>
+T & max(T &arg1, T &arg2) {
+	return arg1 > arg2 ? arg1 : arg2;
+}
+
+#endif
