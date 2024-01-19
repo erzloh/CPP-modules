cf29c23547a8940e06c3984bd432d58ff36c7295
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-01-19 16:23:18 +0100

    Update CPP03

--- CPP03/ex00/srcs/main.cpp
+++ CPP03/ex00/srcs/main.cpp
@@ -1,11 +1,25 @@
 #include "ClapTrap.hpp"
 
 int main() {
-    ClapTrap clapTrap("Test");
+	ClapTrap clapTrap1("ClapTrap1");
+	ClapTrap clapTrap2("ClapTrap2");
 
-    clapTrap.attack("Target");
-    clapTrap.takeDamage(5);
-    clapTrap.beRepaired(5);
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.beRepaired(5);
+
+	clapTrap2.takeDamage(10);
+	clapTrap2.attack("ClapTrap1");
+	clapTrap2.beRepaired(5);
+
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
+	clapTrap1.attack("ClapTrap2");
 
     return 0;
 }
